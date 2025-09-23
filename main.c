f9f5cbe97a030bdcaee02fba0a0bc1c8ecd9d778
diggens42 <frederic.marcel.wahl@gmail.com>
2025-09-23 16:50:06 +0200

    ft_strcpy tests

--- main.c
+++ main.c
@@ -6,11 +6,30 @@
 /*   By: fwahl <fwahl@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/23 15:44:39 by fwahl             #+#    #+#             */
-/*   Updated: 2025/09/23 15:55:03 by fwahl            ###   ########.fr       */
+/*   Updated: 2025/09/23 16:49:45 by fwahl            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libasm.h"
+
+static void tests_strcpy(const char *src, const char *description)
+{
+    char original_dest[256];
+    char custom_dest[256];
+
+    strcpy(original_dest, src);
+    ft_strcpy(custom_dest, src);
+
+    printf("Testing: %s\n", description);
+    printf("  Source: \"%s\"\n", src);
+    printf("  Expected: \"%s\"\n", original_dest);
+    printf("  Actual:   \"%s\"\n", custom_dest);
+    printf("  Result:   %s\n\n",
+        (strcmp(original_dest, custom_dest) == 0) ? "PASS ✓" : "FAIL ✗");
+
+    assert(strcmp(original_dest, custom_dest) == 0);
+}
+
 
 static void tests_strlen(const char *str,  const char *description)
 {
@@ -29,7 +48,6 @@ static void tests_strlen(const char *str,  const char *description)
 int main()
 {
     printf("=== Testing ft_strlen vs standard strlen ===\n\n");
-
     tests_strlen("", "Empty string");
     tests_strlen("H", "Single character");
     tests_strlen("Hi", "Two characters");
@@ -38,6 +56,17 @@ int main()
     tests_strlen("This is a longer string to test", "Longer string");
     tests_strlen("String with numbers 123456", "String with numbers");
     tests_strlen("Special chars: !@#$%^&*()", "Special characters");
+
+
+    printf("=== Testing ft_strcpy vs standard strcpy ===\n\n");
+    tests_strcpy("", "Empty string");
+    tests_strcpy("H", "Single character");
+    tests_strcpy("Hi", "Two characters");
+    tests_strcpy("Hello", "Normal word");
+    tests_strcpy("Hello, World!", "String with punctuation");
+    tests_strcpy("This is a longer string to test", "Longer string");
+    tests_strcpy("String with numbers 123456", "String with numbers");
+    tests_strcpy("Special chars: !@#$%^&*()", "Special characters");
 
     return (0);
 }
