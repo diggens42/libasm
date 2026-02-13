437d939377b41486c5f5bdcd4f48c471f7e971c2
Wahl <fredericmarcel.wahl@exyte.net>
2026-02-13 13:50:46 +0100

    ft_read

---
+++ ft_read.s
@@ -0,0 +1,18 @@
+section .text
+    global ft_read
+    extern __errno_location
+
+ft_read:
+    mov     rax, 0              ; syscall number for read
+    syscall
+    cmp     rax, 0
+    jl      .error
+    ret
+
+.error:
+    neg     rax
+    push    rax
+    call    __errno_location wrt ..plt
+    pop     qword [rax]
+    mov     rax, -1
+    ret
